7fb8f0ee0e7d1c3aa137c0c2d7527e061af9b33b
linjingfu <jingfu.lin@jyquant.com.cn>
2023-08-23 10:08:10 +0800

    fix home btn style

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -2,7 +2,7 @@
   <v-app-bar flat :border="true" density="compact" color="primary">
     <template #title>
       <v-container class="d-flex align-center pa-2 justify-space-between">
-        <v-btn class="mr-5 text-h6" to="/">在线工具</v-btn>
+        <v-btn class="mr-5 text-h6" variant="text" @click="router.push('/')">在线工具</v-btn>
         <v-btn icon="mdi-magnify"/>
       </v-container>
     </template>
@@ -10,5 +10,7 @@
 </template>
 
 <script setup>
+import {useRouter} from 'vue-router'
+const router = useRouter()
 
 </script>
