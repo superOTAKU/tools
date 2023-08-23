32aa1189074774ad26df91604f9d7f673b116d12
linjingfu <jingfu.lin@jyquant.com.cn>
2023-08-23 09:53:02 +0800

    fix home page link

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -2,9 +2,13 @@
   <v-app-bar flat :border="true" density="compact" color="primary">
     <template #title>
       <v-container class="d-flex align-center pa-2 justify-space-between">
-        <v-btn class="mr-5 text-h6" href="/">在线工具</v-btn>
+        <v-btn class="mr-5 text-h6" to="/">在线工具</v-btn>
         <v-btn icon="mdi-magnify"/>
       </v-container>
     </template>
   </v-app-bar>
 </template>
+
+<script setup>
+
+</script>
