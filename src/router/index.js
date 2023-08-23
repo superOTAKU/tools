321d0c90e190ba9ca3e2d538720cffd821af0d8d
linjingfu <jingfu.lin@jyquant.com.cn>
2023-08-23 13:42:26 +0800

    seo optimise

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import "nprogress/nprogress.css"
 NProgress.configure({
   easing: "ease", // 动画方式
   speed: 500, // 递增进度条的速度
-  showSpinner: true, // 是否显示加载ico
+  showSpinner: false, // 是否显示加载ico
   trickleSpeed: 200, // 自动递增间隔
   minimum: 0.3 // 初始化时的最小百分比
 });
@@ -34,6 +34,11 @@ const routes = [
         name: 'ImageTransformer',
         component: () => import('@/views/image/transformer/index.vue')
       },
+      {
+        path: 'develop/crypto',
+        name: 'Crypto',
+        component: () => import('@/views/develop/crypto/index.vue')
+      }
     ],
   },
   {
---
+++ src/views/develop/crypto/index.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <ToolsBreadcrumb :tool-id="3"/>
+</div>
+<v-sheet class="pa-4 ma-4" rounded color="white">
+  <v-textarea label="内容" auto-grow variant="solo" v-model="content"></v-textarea>
+  <div class="d-flex">
+    <v-btn color="primary" @click="onBase64EncodeBtnClicked" class="mr-2">Base64编码</v-btn>
+    <v-btn color="primary" @click="onBase64DecodeBtnClicked">Base64解码</v-btn>
+  </div>
+</v-sheet>
+</template>
+
+<script setup>
+  import ToolsBreadcrumb from '@/components/ToolBreadcrumbs.vue'
+
+  import {ref} from 'vue'
+  import CryptoJS from 'crypto-js'
+
+  const content = ref('')
+
+  const onBase64EncodeBtnClicked = () => {
+    content.value = CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(content.value))
+  }
+
+  const onBase64DecodeBtnClicked = () => {
+    content.value = CryptoJS.enc.Base64.parse(content.value).toString(CryptoJS.enc.Utf8)
+  }
+</script>
